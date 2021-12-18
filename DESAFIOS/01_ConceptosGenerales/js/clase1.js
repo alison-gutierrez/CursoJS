036a86a2bf813d15903be1b8d5e8bc586d01a0ec
alison-gutierrez <guti.adelma@gmail.com>
2021-12-18 02:32:42 -0300

    se agrega proyecto

---
+++ DESAFIOS/01_ConceptosGenerales/js/clase1.js
@@ -0,0 +1,4 @@
+    let unNumero = prompt("Ingresar un numero");
+    let otroNumero = prompt ("Ingresar otro numero");
+    let suma = parseInt(unNumero)  + parseInt(otroNumero);
+    alert ("La suma es: " + suma);
---
+++ DESAFIOS/02_ControlDeFlujos/js/clase2.js
@@ -0,0 +1,9 @@
+let edad = prompt("Ingresar numero entre 10 y 50");
+
+if(parseInt(edad)> 10 && parseInt(edad)<50){
+    alert ("El número se encuentra entre 10 y 50");
+}
+else{
+    alert("El número no se encuentra dentro de los parametros");
+}
+
